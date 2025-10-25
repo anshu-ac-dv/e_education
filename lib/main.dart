400a9d8229e012b2c0fe57d8f0f34903328f4c43
Anshu Kumar <anshu.ac.dv@gmail.com>
2025-10-25 17:53:42 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:e_education/onboarding.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Course Wala',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      //home: ,
+      home: Onboarding(),
     );
   }
 }
---
+++ lib/onboarding.dart
@@ -0,0 +1,95 @@
+import 'package:e_education/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+void main() {
+  runApp(const Onboarding());
+}
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IntroductionScreen(
+        globalBackgroundColor: Colors.white,
+        scrollPhysics: BouncingScrollPhysics(),
+        pages: [
+          PageViewModel(
+            titleWidget: Text(
+              "Introduction",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            body:
+                "With the Course Wala app, your learning journey is in your hands. Explore a wide variety of courses, from competitive exam prep to professional development and creative skills. Our easy-to-use interface, expert instructors, and flexible learning options make quality education accessible and affordable for everyone",
+            image: Image.asset("images/first.jpg", height: 400, width: 400),
+          ),
+          PageViewModel(
+            titleWidget: Text(
+              "This is first title",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            body:
+                "Write the more description of the page. Write the more description of the page. Write the more description of the page. Write the more description of the page. Write the more description of the page. ",
+            image: Image.asset("images/second.jpg", height: 400, width: 400),
+          ),
+          PageViewModel(
+            titleWidget: Text(
+              "This is first title",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            body:
+                "Write the more description of the page. Write the more description of the page. Write the more description of the page. Write the more description of the page. Write the more description of the page. ",
+            image: Image.asset("images/thired.jpg", height: 400, width: 400),
+          ),
+        ],
+        onDone: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => HomeScreen()),
+          );
+        },
+        onSkip: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => HomeScreen()),
+          );
+        },
+        showSkipButton: true,
+        skip: Text(
+          "skip",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            color: Colors.blue,
+          ),
+        ),
+        next: Icon(Icons.arrow_forward, color: Colors.blue),
+        done: Text(
+          "done",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            color: Colors.blue,
+          ),
+        ),
+        dotsDecorator: DotsDecorator(
+          size: Size.square(10.0),
+          activeSize: Size(20.0, 10.0),
+          color: Colors.black,
+          activeColor: Colors.blue,
+          spacing: EdgeInsets.symmetric(horizontal: 3.0),
+          activeShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
